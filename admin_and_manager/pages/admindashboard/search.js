c88a53831ebd105795595b47857ba2566ff9bb0b
MorolShohan <shohan.aiubcse@gmail.com>
2023-12-21 02:29:49 +0600

    committing till search

---
+++ admin_and_manager/pages/admindashboard/search.js
@@ -0,0 +1,10 @@
+import AdminSearch from '../Layout/searchadminid';
+const SearchPage = () => {
+    return (
+      <div>
+        <SearchPage />
+      </div>
+    );
+  };
+ 
+  export default AdminSearch;
